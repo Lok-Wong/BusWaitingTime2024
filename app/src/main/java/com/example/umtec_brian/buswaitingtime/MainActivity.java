af9f1eadb060e8ad74c12aead9d183838554846c
周光庆 <niconchow@gmail.com>
2024-10-31 12:00:05 +0800

    改了個bug,關於橫琴路線——其它的bug
    2024/10/31

--- app/src/main/java/com/example/umtec_brian/buswaitingtime/MainActivity.java
+++ app/src/main/java/com/example/umtec_brian/buswaitingtime/MainActivity.java
@@ -33,6 +33,7 @@ import android.text.TextWatcher;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
